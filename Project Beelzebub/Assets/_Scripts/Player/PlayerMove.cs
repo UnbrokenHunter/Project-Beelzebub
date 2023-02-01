b5be0ac72d312f2125660210fe4abf110953de3c
Hunter <huntersfred@gmail.com>
2023-02-01 16:32:47 -0500

    Added Dev Tilemap and character

---
+++ Project Beelzebub/Assets/_Scripts/Player/PlayerMove.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace ProjectBeelzebulb
+{ 
+    public class PlayerMove : MonoBehaviour
+    {
+        private void Awake()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+    }
+}
