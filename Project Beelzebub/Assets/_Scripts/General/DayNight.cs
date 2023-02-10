1e53fd5ae5e8ee37f667f5c074666a6a43c794e0
Hunter <huntersfred@gmail.com>
2023-02-09 23:45:25 -0500

    Day Night Added

--- Project Beelzebub/Assets/_Scripts/General/DayNight.cs
+++ Project Beelzebub/Assets/_Scripts/General/DayNight.cs
@@ -8,42 +8,49 @@ namespace ProjectBeelzebub
 {
     public class DayNight : MonoBehaviour
     {
+		[Title("Speed/Length")]
+		[SerializeField] private float dayNightLength = 2;
+
+		[SerializeField] private float sunRiseSpeed = 1;
+		[SerializeField] private float sunSetSpeed = 1;
+		[SerializeField] private AnimationCurve dayCurve;
+
+
 		[Title("Day/Night")]
-		[SerializeField] private float dayLength = 2;
-
-		[SerializeField] private float daySpeed = 1;
-		[SerializeField] private float nightSpeed = 1;
-
 		[SerializeField] private bool isDay = true;
+
+		[SerializeField] private float dayReq = .5f;
 
 		[Space]
 
-		[SerializeField] private float intensity;
-		[SerializeField] private float lightLevel = 1;
+
+		[Title("Other")]
+		[SerializeField] private float intensity = 1;
 		[SerializeField] private float min = 1;
-		[SerializeField] private Light2D light;
+		[SerializeField] private Light2D light2D;
 
 		[SerializeField] private float timer = 0;
 
 		private void Update()
 		{
 			if (isDay)
-				timer += Time.deltaTime * daySpeed;
+				timer += Time.deltaTime * sunRiseSpeed * dayCurve.Evaluate(timer);
 
 			else
-				timer -= Time.deltaTime * nightSpeed;
+				timer -= Time.deltaTime * sunSetSpeed * dayCurve.Evaluate(timer);
 
 
-			if (timer > dayLength)
+			if (timer > dayNightLength)
 				isDay = false;
 			
 			if(timer < 0)
 				isDay = true;
 
-			var amt = min + (timer / 10) * lightLevel;
+			var amt = (timer / 10);
 			intensity = Mathf.InverseLerp(0, 1, amt);
+			intensity = Mathf.Clamp(amt, min, 1);
 
-			light.intensity = intensity;
+            light2D.intensity = intensity;
 
 		}
 
