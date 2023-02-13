3ec2bdc6b710a29f07ba55ba9bd9e158aac10cb9
Hunter <huntersfred@gmail.com>
2023-02-12 23:24:52 -0500

    Build

--- Project Beelzebub/Assets/_Scripts/Scene/LevelManager.cs
+++ Project Beelzebub/Assets/_Scripts/Scene/LevelManager.cs
@@ -1,8 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Sirenix.OdinInspector;
-using UnityEditor.Sequences;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
